c54eaa81915722febdedaaf923b2197dfe1d1c17
Ken Ishii <riguveda44@gmail.com>
2022-08-07 15:31:18 +0900

    ユーザー登録をログイン時に変更

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django .views import generic
+from .models import User
 
 # Create your views here.
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('analytics:base_dashboard')
     template_name = 'accounts/signup.html'
+
+class UserList(generic.ListView):
+    template_name = "accounts/user_list.html"
+    model = User
