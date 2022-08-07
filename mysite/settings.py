42c5f0c794765d1aefaf2d4ce05532c25080f57a
Ken Ishii <riguveda44@gmail.com>
2022-08-07 16:23:16 +0900

    ユーザーの登録項目を変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'accounts.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
